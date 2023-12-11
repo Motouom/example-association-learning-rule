7c947642f77134398c1570da5ee57e800fa105f6
Motouom_Wandjeu_Victoire_Josue <motouomvictor@gmail.com>
2023-12-11 09:19:07 +0100

    feature(backend): implemented a code to calculate the support-value of a an itemset in a dataset

--- docs/support.md
+++ docs/support.md
@@ -23,3 +23,24 @@ Output: Support value for Itemset
 3. Support = count / Total number of transactions in Dataset D
 4. Return Support
 ```
+
+--In this code [LINK TEXT](../src/support.py), the function support is used to find the frequency of a given item set in a given dataset.
+
+--Firstly, it checks if the dataset is empty. If it is, it throws an error message.
+
+--If the dataset is not empty, it initializes a variable item_occurrence to keep track of the number of occurrences of the item set in the dataset.
+
+--Next, it iterates through each list (itemset) in the dataset.
+
+--Inside the loop, it checks if the elements of the given item set are present in the current list (itemset).
+
+--If both elements are present, it increments the item_occurrence variable by 1.
+
+--After iterating through all the lists in the dataset, it calculates the support value by dividing the item_occurrence variable by the total number of lists in the dataset.
+
+--Hence, it prints the support value for the given item set.
+
+--This code provides an alternative approach to finding the support value of a given item set in a dataset using a different approach than the support function provided in the question.
+
+
+
--- src/support.py
+++ src/support.py
@@ -1,25 +1,45 @@
-from typing import List
+from test.test_support import *
 
-itemset_type = frozenset
-dataset_type = List[List[object]]
+itemset_type = list[object]
+dataset_type = list[list[object]]
+
 
 def support(itemsets: dataset_type, data_set: itemset_type) -> float:
     """
-    To find the frequency of itemsets in the dataset.
+    To find the frequency of item sets in the dataset.
     """
-    # initializing the counter to keep track of the occurrence in the list
-    count = 0
-
-    total_transactions = len(data_set)
-
-    # looping through the entire dataset for occurrence of itemset
-    for transaction in data_set:
-        if itemsets.issubset(transaction):
-            count = count + 1
-
-    # implementing support method
-    support_value = count / total_transactions
-
     return support_value
 
 
+dataset = [
+    ['bread', 'milk'],
+    ['bread', 'diaper', 'beer', 'eggs'],
+    ['milk', 'diaper', 'beer', 'cola'],
+    ['bread', 'milk', 'diaper', 'beer'],
+    ['bread', 'milk', 'diaper', 'cola']
+]
+itemset = ['bread', 'milk']
+
+"""
+checking if the dataset is empty or not to defore continue
+"""
+if len(dataset) != 0:
+
+    item_occurrence = 0
+    for items in range(len(dataset)):
+        """
+    comparing the elements in the itemset to those in the dataset to see the list that satisfies the itemsets
+    """
+
+        if itemset[0] in dataset[items] and itemset[1] in dataset[items]:
+            item_occurrence = item_occurrence + 1
+    """
+    throwing an error message if the dataset is empty
+    """
+else:
+    print(f"error:{dataset} is empty")
+
+support = item_occurrence / len(dataset)
+
+
+print(f"support value for: {itemset} is {support}")
